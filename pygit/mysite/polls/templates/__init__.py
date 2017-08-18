887362ec65f84355c94c31dc3082ec0adaac5dfd
xiang.zhang <359407479@qq.com>
2017-08-18 18:07:19 +0800

    new django project

---
+++ pygit/mysite/polls/templates/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+# Title: 
+# Author: xiang.zhang
+# Date: 2017/8/18
+# Description: 
+
+
+
+
+
+
+
+
+
+
+
+
+
--- pygit/mysite/polls/urls.py
+++ pygit/mysite/polls/urls.py
@@ -8,10 +8,14 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^webtest/', views.webtest, name='webtest'),
-	url(r'^Inetest/', views.Inetest, name='Inetest')
+	url(r'^Inetest/', views.Inetest, name='Inetest'),
+	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
 
 
